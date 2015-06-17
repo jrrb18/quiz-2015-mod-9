207bdd44588bf4d6a8ea9b77e56eb993fb1373ab
Jose R. Rojas B <jrrb18@gmail.com>
2015-06-17 02:10:48 -0400

    Creacion Pagina de Creditos

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' });
 });
 
+router.get('/author', function(req, res, next) {
+  res.render('author', { nombre_autor: 'Jose R. Rojas B.', foto_autor: 'images/autor.jpg', video_autor: 'videos/about_me.mp4' });
+});
+
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
 
